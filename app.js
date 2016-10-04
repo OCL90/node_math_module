1eba5b959484aa13c3daeab78ef59c01227d6da4
Oscar Cortazar-Luebbert <oscar_cortazar@hotmail.com>
2016-10-04 13:45:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var mathlib = require('./mathlib');
+mathlib.add(7,3);
+mathlib.multiply(5,5);
+mathlib.square(2);
+mathlib.random(4,10);
---
+++ mathlib.js
@@ -0,0 +1,16 @@
+module.exports = function (){
+  return {
+    add: function(num1, num2) { 
+         console.log("The sum of "+ num1 + " and "+ num2 +" is", num1 + num2)
+       },
+    multiply: function(num1, num2) {
+         console.log("The product of "+ num1 + " and "+ num2 +" is" , num1 * num2)
+       },
+    square: function(num) {
+         console.log("The square of " + num + " is", num * num)
+    },
+    random: function(num1, num2){
+      console.log("Random number between " +num1+ " and "+num2+" is", Math.floor(Math.random()*(num2-num1+1))+num1)
+    }
+  }
+}();
